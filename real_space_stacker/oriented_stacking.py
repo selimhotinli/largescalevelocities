3e6291d3a1a82cb36963e0ea057b51eeb424848e
Selim Hotinli <selim.hotinli14@imperial.ac.uk>
2019-11-06 23:32:28 -0500

    Update oriented_stacking.py

--- real_space_stacker/oriented_stacking.py
+++ real_space_stacker/oriented_stacking.py
@@ -1,3 +1,6 @@
+#### produces code to be updated and under development
+#### contact: selim.hotinli14@imperial.ac.uk
+#### produce stacked imagines of CMB around halos
 import sys, platform, os
 from sys import argv
 import numpy as np
